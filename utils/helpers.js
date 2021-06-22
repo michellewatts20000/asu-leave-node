5e5d5c8d226836c880718ecfbe5b4f1cf9aa3c41
Michelle Watts <watts.e.michelle@gmail.com>
2021-06-22 15:12:20 +1000

    weeks to years

--- utils/helpers.js
+++ utils/helpers.js
@@ -4,10 +4,8 @@ module.exports = {
     return date.toLocaleDateString();
   },
 
-  getTotal: function (unpaidHours) {
-    const reducer = (accumulator, currentValue) => accumulator + currentValue;
-    total = unpaidHours.reduce(reducer);
-    return total;
+  weeksToYears: function (x) {
+    return (x / 52).toFixed(1)
   },
 
   numberWithCommas: function (x) {
